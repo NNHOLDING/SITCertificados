1fcdfefe46e78460f411073f69c95159a0dec6d6
NNHOLDING <nholdingsolutions@gmail.com>
2025-07-06 16:35:54 -0600

    Primer commit: app de certificados con Streamlit

---
+++ app.py
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Certificados Unimar</title>
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="http://localhost:8501"></iframe>
+</body>
+</html>
